17ff2596a7f36206b69ca9e0a62e2dac630e726e
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-06-12 17:13:10 -0400

    Start adding mock data

---
+++ app.js
@@ -0,0 +1,14 @@
+const MOCK_RAID_TEAMS = {
+  'raidTeams': [
+    {
+      'id': '1111111',
+      'teamName': 'Straight-up B-Team',
+      'members': [
+        {
+          'playerName':'Bob',
+          'playerClass': 'Paladin'
+        }
+      ]
+    }
+  ]
+};
